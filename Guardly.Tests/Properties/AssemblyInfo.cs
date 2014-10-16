58870d22bb4e57ef330c1b7ee4a9fb66396ea502
Evgeny Nazarov <e.v.nazarov@mail.ru>
2014-10-16 08:23:12 +0000

    Fix AssemblyInfo files

--- Guardly.Tests/Properties/AssemblyInfo.cs
+++ Guardly.Tests/Properties/AssemblyInfo.cs
@@ -37,20 +37,14 @@
 // [This is the BSD license, see http://www.opensource.org/licenses/bsd-license.php]
 
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Guardly.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Guardly.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyProduct("Guardly")]
+[assembly: AssemblyCopyright("Copyright © Evgeny Nazarov 2014")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -59,16 +53,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4569498d-29df-4507-b0ac-58f865660f7f")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
